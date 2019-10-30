a4d2d89ac6f729600dfb21678b530204fb12d573
Valerio <lhuancah@uni.pe>
2019-10-30 11:47:26 -0500

    cambios en funciones

---
+++ include/funciones.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math>
+
+int main(){
+        int i;
+        for (int i = 0; i < 100; i++)
+        {
+                printf("%d", i);
+        }
+        return 0;
+}
+
